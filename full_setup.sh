1761a22364d25240e2efa5cbf89e0b45de2db520
Fillipe Goulart <fillipe.gsm@tutanota.com>
2024-11-13 10:08:20 -0300

    Run script with sudo privileges
    The current approach is not very nice because, during the
    installation of some packages, we lose sudo privileges and
    require to type the password again.
    
    And if I am not paying attention everytime the script crashes.
    
    I changed it so it can be run with `sudo` directly.
    
    Of course, commands that don't need it are run with my regular user
    instead.

--- full_setup.sh
+++ full_setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# IT should be run with `sudo`, but some commands will not require such permissions and will be dropped
+
 echo "=========================================================================="
 echo "Updating system packages..."
-sudo dnf -y update
+dnf -y update
 echo "... done."
 
 echo "=========================================================================="
 echo "Installing packages"
 echo "- Package managers..."
-sudo dnf -y install pipx git
+dnf -y install pipx git
 echo "... done."
 
 echo "- Utilities..."
@@ -20,17 +22,17 @@ dnf -y install libvirt virt-manager qemu
 echo "... done."
 
 echo "- Neovim..."
-sudo dnf -y install neovim gcc fd-find nodejs cargo python3-pip
+dnf -y install neovim gcc fd-find nodejs cargo python3-pip
 echo "... done."
 
 echo "- Latex (installing medium set of packages)..."
-sudo dnf -y install texlive-scheme-medium
+dnf -y install texlive-scheme-medium
 echo "... done."
 
 echo "- Programming..."
-sudo dnf -y install httpie docker docker-compose R-devel
-sudo dnf copr enable atim/lazygit -y
-sudo dnf -y install lazygit
+dnf -y install httpie docker docker-compose R-devel
+dnf copr enable atim/lazygit -y
+dnf -y install lazygit
 echo "... done."
 
 echo "- Pipx packages..."
@@ -46,10 +48,9 @@ echo "... done."
 
 echo "=========================================================================="
 echo "Copying .config files (with symbolic links)"
-ln -sf $PWD/config/sway/config ~/.config/sway/config
-ln -sf $PWD/config/waybar/config ~/.config/waybar/config
-ln -sf $PWD/config/kitty ~/.config/kitty
-# cp --archive --verbose --recursive --update ./config/. ~/.config/
+sudo -u $USER ln -sf $PWD/config/sway/config ~/.config/sway/config
+sudo -u $USER ln -sf $PWD/config/waybar/config ~/.config/waybar/config
+sudo -u $USER ln -sf $PWD/config/kitty ~/.config/kitty
 
 # echo "Copying .xonshrc configuration"
 # cp --verbose --update ./xonsh/.xonshrc ~/
@@ -64,7 +65,7 @@ nvim_path="$HOME/.config/nvim"
 if [ -d "$nvim_path" ]; then
 	echo "...neovim config already exists. Skipping."
 else
-	git clone "https://github.com/fillipe-gsm/kickstart.nvim.git" "$nvim_path"
+	sudo -u $USER git clone "https://github.com/fillipe-gsm/kickstart.nvim.git" "$nvim_path"
 	echo "...open neovim and see everything being installed."
 fi
 
@@ -72,33 +73,33 @@ echo "==========================================================================
 echo "Adding multimedia support via RPM Fusion..."
 
 # Enabling the non-free repo
-sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Switch to full ffmpeg
-sudo dnf -y swap ffmpeg-free ffmpeg --allowerasing
+dnf -y swap ffmpeg-free ffmpeg --allowerasing
 
 # Install additional codecs
-sudo dnf -y groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-sudo dnf -y groupupdate sound-and-video
+dnf -y groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+dnf -y groupupdate sound-and-video
 
 echo "... done."
 
 echo "=========================================================================="
 echo "Installing Mullvad VPN"
-sudo dnf config-manager --add-repo https://repository.mullvad.net/rpm/stable/mullvad.repo
-sudo dnf install -y mullvad-vpn
+dnf config-manager --add-repo https://repository.mullvad.net/rpm/stable/mullvad.repo
+dnf install -y mullvad-vpn
 
 echo "... done."
 
 echo "=========================================================================="
 echo "Configuring docker..."
 
-sudo systemctl enable docker  # enable it on startup
-sudo systemctl start docker  # start it now
+systemctl enable docker  # enable it on startup
+systemctl start docker  # start it now
 
 # Add current user to "docker" group so we don't need `sudo` to use it
-sudo groupadd docker
-sudo gpasswd -a ${USER} docker
+groupadd docker
+gpasswd -a ${USER} docker
 # newgrp docker  # to log-in if required
 
 echo "... done."
